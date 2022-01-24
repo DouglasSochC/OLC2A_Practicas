663264c8d5ac07076a56c58dacf7a1147a2cdbdb
DouglasSochC <sochdouglas1623@gmail.com>
2022-01-24 15:03:02 -0600

    Practica 1 realizada correctamente

---
+++ Practica1/Ejercicio2/src/ejercicio2/Ejercicio2.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio2;
+
+import java.io.FileInputStream;
+
+/**
+ *
+ * @author douglas
+ */
+public class Ejercicio2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        interpretar("entrada2.txt");
+    }
+    
+    /**
+     * Método que interpreta el contenido del archivo que se encuentra en el path
+     * que recibe como parámentro
+     * @param path ruta del archivo a interpretar
+     */
+    private static void interpretar(String path) {
+        analizadores.Sintactico pars;
+        try {
+            pars=new analizadores.Sintactico(new analizadores.Lexico(new FileInputStream(path)));
+            pars.parse();        
+        } catch (Exception ex) {
+            System.out.println("Error fatal en compilación de entrada.");
+            System.out.println("Causa: "+ex.getCause());
+        } 
+    }
+    
+}
